330e590319f5f1740ac498e47aed5f87669dcf76
Draconik513 <draconik514@gmail.com>
2026-06-14 15:13:22 +0800

    bar

--- src/pages/BirthdayCountdown.jsx
+++ src/pages/BirthdayCountdown.jsx
@@ -103,7 +103,7 @@ Kita hitung mundur</h2>
 
         <div className="bg-white p-6 rounded-xl shadow-lg mb-8 border border-pink-100">
           <h3 className="text-xl font-semibold text-purple-600 mb-2">
-            📅 23 Juni 2026
+            📅 22 Juni 2026
           </h3>
           <p className="text-pink-600">
              cieeeee ulang taun yaaa 😂
